510646b49dfa7b69eb1354b277ec3233a4ca7d72
Muhammad Hamza <160301787+Hamza2171002@users.noreply.github.com>
2024-11-12 16:00:14 +0500

    Add files via upload

--- Milestone_4/script.ts
+++ Milestone_4/script.ts
@@ -1,42 +1,73 @@
-// get referances to the form  and display area
+let myName: any = document.getElementById('name')
+let desig: any = document.getElementById('desig')
+let Phone: any = document.getElementById('Phone')
+let email: any = document.getElementById('email')
+let add: any = document.getElementById('add')
+let pass1: any = document.getElementById('pass1')
+let deg: any = document.getElementById('deg')
+let uni: any = document.getElementById('uni')
+let pass2: any = document.getElementById('pass2')
+let deg2: any = document.getElementById('deg2')
+let uni2: any = document.getElementById('uni2')
+let skill1: any = document.getElementById('skill1')
+let skill2: any = document.getElementById('skill2')
+let skill3: any = document.getElementById('skill3')
+let lang : any= document.getElementById('lang')
+let lang2: any = document.getElementById('lang2')
+let styear: any = document.getElementById('styear')
+let endyear: any = document.getElementById('endyear')
+let company: any = document.getElementById('company')
+let comlocation: any = document.getElementById('comlocation')
+let jobtitle: any = document.getElementById('jobtitle')
+let achv1: any = document.getElementById('achv1')
+let achv2: any = document.getElementById('achv2')
+let achv3: any = document.getElementById('achv3')
+let pic: any = document.getElementById('pic')
 
-const form = document.getElementById('resume-form') as HTMLFormElement
-const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement
 
-//handle form submission
-form.addEventListener('submit', (event: Event) => {
-    event.preventDefault();  // prevent page reload
 
-    //collect input values
-    const name = (document.getElementById('name') as HTMLInputElement).value
-    const email = (document.getElementById('email') as HTMLInputElement).value
-    const phone = (document.getElementById('phone') as HTMLInputElement).value
-    const education = (document.getElementById('education') as HTMLSelectElement).value
-    const experience = (document.getElementById('experience') as HTMLSelectElement).value
-    const skills = (document.getElementById('skills') as HTMLTextAreaElement).value
+let submitBtn = document.getElementById('submitBtn')
+let form = document.getElementById('form')
 
-    //generate the resume content dynamically
-    const resumeHTML = `
-    <h2><b>Editable Resume</b></h2>
-    <h3>Personal Information</h3>
-    <p><b>Name:</b><span contenteditable="true">${name}</span></p>
-    <p><b>Email:</b><span contenteditable="true">${email}</span></p>
-    <p><b>Phone:</b><span contenteditable="true">${phone}</span></p>
-    
-    <h3>Education</h3>
-    <p contenteditable="true">${education}</p>
-    
-    <h3>Experience</h3>
-    <p contenteditable="true">${experience}</p>
-    
-    <h3>Skills</h3>
-    <p contenteditable="true">${skills}</p>`
+form?.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-    //display the generate resume
+    localStorage.setItem('name', myName.value)
+    localStorage.setItem('desig', desig.value)
+    localStorage.setItem('Phone', Phone.value)
+    localStorage.setItem('email', email.value)
+    localStorage.setItem('add', add.value)
+    localStorage.setItem('pass1', pass1.value)
+    localStorage.setItem('deg', deg.value)
+    localStorage.setItem('uni', uni.value)
+    localStorage.setItem('pass2', pass2.value)
+    localStorage.setItem('deg2', deg2.value)
+    localStorage.setItem('uni2', uni2.value)
+    localStorage.setItem('skill1', skill1.value)
+    localStorage.setItem('skill2', skill2.value)
+    localStorage.setItem('skill3', skill3.value)
+    localStorage.setItem('lang', lang.value)
+    localStorage.setItem('lang2', lang2.value)
+    localStorage.setItem('styear', styear.value)
+    localStorage.setItem('endyear', endyear.value)
+    localStorage.setItem('company', company.value)
+    localStorage.setItem('comlocation', comlocation.value)
+    localStorage.setItem('jobtitle', jobtitle.value)
+    localStorage.setItem('achv1', achv1.value)
+    localStorage.setItem('achv2', achv2.value)
+    localStorage.setItem('achv3', achv3.value)
 
-    if(resumeDisplayElement){
-        resumeDisplayElement.innerHTML = resumeHTML;
-    }else{
-        console.error('The resume display element is missing.');
+
+    if(pic.files && pic.files[0]){
+        let reader = new FileReader()
+        reader.addEventListener("load", () => {
+            let textImg:any = reader.result
+            localStorage.setItem('profile_pic', textImg)
+        })
+        reader.readAsDataURL(pic.files[0])
     }
+
+    window.location.href = "./resume/resume.html"
 })
+
+
